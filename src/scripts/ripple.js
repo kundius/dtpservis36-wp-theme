c3bd0f23ffc35547fc692152c9af4c8592bccda4
Kundius Ruslan <kundius.ruslan@gmail.com>
2022-04-14 13:02:36 +0300

    deploy

--- src/scripts/ripple.js
+++ src/scripts/ripple.js
@@ -1,5 +1,3 @@
-const primaryButtons = document.querySelectorAll(".ui-button-primary") || [];
-
 class RippleStyleAttributes {
   constructor(width, height, posX, posY) {
     this.width = width <= height ? height : width;
@@ -9,21 +7,32 @@ class RippleStyleAttributes {
   }
 }
 
-primaryButtons.forEach((primaryButton) => {
-  const tag = primaryButton.querySelector(".ui-button-primary__ripple");
+function initForClassName(className) {
+  const primaryButtons = document.querySelectorAll(`.${className}`) || [];
 
-  if (!tag) return;
+  primaryButtons.forEach((primaryButton) => {
+    const tag = primaryButton.querySelector(`.${className}__ripple`);
 
-  primaryButton.addEventListener("click", function (ev) {
-    let pos = this.getBoundingClientRect();
-    let width = this.offsetWidth;
-    let height = this.offsetHeight;
-    let posX = ev.pageX - pos.left;
-    let posY = ev.pageY - pos.top;
-    let rippleStyleAttr = new RippleStyleAttributes(width, height, posX, posY);
-    tag.style.width = rippleStyleAttr.width + "px";
-    tag.style.height = rippleStyleAttr.height + "px";
-    tag.style.top = rippleStyleAttr.top + "px";
-    tag.style.left = rippleStyleAttr.left + "px";
+    if (!tag) return;
+
+    primaryButton.addEventListener("click", function (ev) {
+      let pos = this.getBoundingClientRect();
+      let width = this.offsetWidth;
+      let height = this.offsetHeight;
+      let posX = ev.pageX - pos.left;
+      let posY = ev.pageY - pos.top;
+      let rippleStyleAttr = new RippleStyleAttributes(
+        width,
+        height,
+        posX,
+        posY
+      );
+      tag.style.width = rippleStyleAttr.width + "px";
+      tag.style.height = rippleStyleAttr.height + "px";
+      tag.style.top = rippleStyleAttr.top + "px";
+      tag.style.left = rippleStyleAttr.left + "px";
+    });
   });
-});
+}
+
+initForClassName("ui-button-primary");
