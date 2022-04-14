870e6ff3bc8114f99a89073fca7e59b891f4b6fa
Kundius Ruslan <kundius.ruslan@gmail.com>
2022-04-14 16:06:46 +0300

    deploy

--- inc/Services/Acf.php
+++ inc/Services/Acf.php
@@ -25,6 +25,25 @@ class Acf implements Service
      */
     public function boot(Service_Container $container): void
     {
+        \add_action('acf/init', [$this, 'on_init']);
+    }
+
+    /**
+     * @return string
+     */
+    public function get_service_name(): string
+    {
+        return 'acf';
+    }
+
+    public function on_init(): void
+    {
+        $this->add_options_page();
+        $this->register_acf_fields();
+    }
+
+    public function add_options_page(): void
+    {
         \acf_add_options_page(array(
             'page_title' => 'Параметры',
             'menu_title' => 'Параметры',
@@ -34,12 +53,43 @@ class Acf implements Service
         ));
     }
 
-    /**
-     * @return string
-     */
-    public function get_service_name(): string
+    public function register_acf_fields(): void
     {
-        return 'acf';
+        \acf_add_local_field_group([
+            'key' => 'group_theme_reviews',
+            'title' => 'Отзывы',
+            'fields' => [
+                [
+                    'key' => 'group_theme_reviews_tab',
+                    'label' => 'Отзывы',
+                    'name' => 'theme_reviews_tab',
+                    'type' => 'tab',
+                ],
+                [
+                    'key' => 'group_theme_reviews',
+                    'label' => 'Отзывы',
+                    'name' => 'theme_reviews',
+                    'type' => 'group',
+                    'fields' => [
+                        [
+                            'key' => 'group_theme_reviews_title',
+                            'label' => 'Заголовок',
+                            'name' => 'theme_reviews_title',
+                            'type' => 'text',
+                        ]
+                    ],
+                ],
+            ],
+            'location' => [
+                [
+                    [
+                        'param' => 'options_page',
+                        'operator' => '==',
+                        'value' => 'acf-options',
+                    ],
+                ],
+            ],
+        ]);
     }
 
 }
