113baaaa58abb37d3c44c187d90bb2fb78a4fc07
Kundius Ruslan <kundius.ruslan@gmail.com>
2022-04-14 04:00:45 +0300

    deploy

---
+++ partials/header.php
@@ -0,0 +1,31 @@
+<header class="header">
+  <div class="ui-container ui-container_large header__container">
+    <a href="/" class="header__logo">
+      <img src="<?php bloginfo('template_url') ?>/dist/images/logo.png" alt="<?php bloginfo('name') ?>" />
+    </a>
+    <ul class="header__about">
+      <li>Выезд за 5 минут на место</li>
+      <li>Оформление ДТП в Воронеже</li>
+    </ul>
+    <div class="header__sep-1"></div>
+    <ul class="header__menu">
+      <li>
+        <a href="#">Услуги</a>
+      </li>
+      <li>
+        <a href="#">Преимущества</a>
+      </li>
+      <li>
+        <a href="#">Отзывы</a>
+      </li>
+      <li>
+        <a href="#">Контакты</a>
+      </li>
+    </ul>
+    <div class="header__sep-2"></div>
+    <div class="header__phone">
+      <a href="tel:" class="header__phone-number">+7 (950) 771-1-911</a>
+      <div class="header__phone-time">круглосуточно, без выходных</div>
+    </div>
+  </div>
+</header>
