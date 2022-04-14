bbe544de6d26af05b72ea31bcaa329b545b8e40c
Kundius Ruslan <kundius.ruslan@gmail.com>
2022-04-14 22:43:16 +0300

    deploy

--- src/scripts/reviews.js
+++ src/scripts/reviews.js
@@ -4,42 +4,52 @@ function init (wrapper, reviews) {
   const name = wrapper.querySelector('.reviews-info__name')
   const description = wrapper.querySelector('.reviews-info__desc')
   const date = wrapper.querySelector('.reviews-info__date')
-  const content = wrapper.querySelector('.reviews-preview__content')
+  const excerpt = wrapper.querySelector('.reviews-preview__excerpt')
   const image = wrapper.querySelector('.reviews-preview__figure-image')
 
   let duration = 500
   let active = 0
-  let timer
+  // let timer
+  let animating = false
 
   const renderReview = (index) => {
     wrapper.classList.add('reviews_animation-out')
-    if (timer) {
-      clearTimeout(timer)
-    }
-    timer = setTimeout(() => {
+    // if (timer) {
+    //   clearTimeout(timer)
+    // }
+    animating = true
+    setTimeout(() => {
       name.innerHTML = reviews[index].name
       description.innerHTML = reviews[index].description
       date.innerHTML = reviews[index].date
-      content.innerHTML = reviews[index].excerpt
+      excerpt.innerHTML = reviews[index].excerpt
       image.style.backgroundImage = reviews[index].image ? `url('${reviews[index].image.url}')` : null
       wrapper.classList.remove('reviews_animation-out')
       wrapper.classList.add('reviews_animation-in')
-      if (timer) {
-        clearTimeout(timer)
-      }
-      timer = setTimeout(() => {
+      // if (timer) {
+      //   clearTimeout(timer)
+      // }
+      setTimeout(() => {
         wrapper.classList.remove('reviews_animation-in')
+        animating = false
       }, duration)
     }, duration)
   }
 
   buttonLeft.addEventListener('click', () => {
+    if (animating) return
+
     active = active === 0 ? reviews.length - 1 : active - 1
+
     renderReview(active)
     
   })
+  
   buttonRight.addEventListener('click', () => {
+    if (animating) return
+
     active = active === reviews.length - 1 ? 0 : active + 1
+
     renderReview(active)
   })
 }
