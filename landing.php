5078192b9c78a45360ee66b58d4800d8d2e812a7
Kundius Ruslan <kundius.ruslan@gmail.com>
2022-04-14 11:30:27 +0300

    deploy

--- landing.php
+++ landing.php
@@ -16,7 +16,66 @@ Template Name: Главная
 
       <section class="intro">
         <div class="ui-container">
-          intro
+          <div class="intro-special">
+            <div class="intro-special__title">
+              Спецпредложение!
+            </div>
+            <div class="intro-special__desc">
+              При заявке с сайта <span>оформление европротокола БЕСПЛАТНО!</span>
+            </div>
+          </div>
+
+          <div class="intro__body">
+            <form class="intro-form">
+              <div class="intro-form__title">
+                <span>Оформление ДТП</span>
+                в Воронеже
+              </div>
+              <div class="intro-form__desc">
+                Служба аварийных комиссаров
+              </div>
+              <div class="intro-form__input">
+                <div class="ui-input-phone">
+                  <div class="ui-input-phone__label">Введите ваш номер телефона</div>
+                  <input type="text" class="ui-input-phone__input" name="your-phone" />
+                </div>
+              </div>
+              <div class="intro-form__submit">
+                <button class="ui-button-primary">Вызвать комиссара</button>
+              </div>
+            </form>
+
+            <div class="intro__urgent">
+              Срочно!!!
+            </div>
+          </div>
+
+          <div class="intro-advantages">
+            <div class="intro-advantages__item">
+              <div class="intro-advantages__item-title">
+                Оперативно -
+              </div>
+              <div class="intro-advantages__item-desc">
+                выезд на место за 5 мин
+              </div>
+            </div>
+            <div class="intro-advantages__item">
+              <div class="intro-advantages__item-title">
+                Воронеж -
+              </div>
+              <div class="intro-advantages__item-desc">
+                комиссары во всех районах
+              </div>
+            </div>
+            <div class="intro-advantages__item">
+              <div class="intro-advantages__item-title">
+                Выгодно -
+              </div>
+              <div class="intro-advantages__item-desc">
+                оформление европротокола бесплатно
+              </div>
+            </div>
+          </div>
         </div>
       </section>
 
