9cc30049e5da63af3c347358b0d2fbc42bda0af5
Kundius Ruslan <kundius.ruslan@gmail.com>
2022-04-15 20:58:28 +0300

    deploy

---
+++ inc/Services/Ajax.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace DomenART\Theme\Services;
+
+use DomenART\Theme\Service;
+use DomenART\Theme\Service_Container;
+
+/**
+ * Class Ajax
+ *
+ * @package DomenART\Theme
+ */
+class Ajax implements Service
+{
+
+    /**
+     * @param Service_Container $container
+     */
+    public function register(Service_Container $container): void
+    {
+    }
+
+    /**
+     * @param Service_Container $container
+     */
+    public function boot(Service_Container $container): void
+    {
+        \add_action('wp_enqueue_scripts', [$this, 'myajax_data'], 99);
+
+        \add_action('wp_ajax_get_agreement', [$this, 'get_agreement_callback']);
+        \add_action('wp_ajax_nopriv_get_agreement', [$this, 'get_agreement_callback']);
+    }
+
+    /**
+     * @return string
+     */
+    public function get_service_name(): string
+    {
+        return 'acf';
+    }
+
+    public function myajax_data(): void
+    {
+        \wp_localize_script('twentyfifteen-script', 'myajax',
+            array(
+                'url' => \admin_url('admin-ajax.php'),
+            )
+        );
+    }
+
+    public function get_agreement_callback(): void
+    {
+        $whatever = intval($_POST['whatever']);
+
+        echo $whatever + 10;
+
+        // выход нужен для того, чтобы в ответе не было ничего лишнего, только то что возвращает функция
+        wp_die();
+    }
+
+}
